98c8f8b7a479be7b185d3d41319fd035342a67fb
Adryelle Araujo <adrytvd@gmail.com>
2023-06-24 18:22:55 -0300

    construindo container lateral e filtros

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import * as React from 'react';
 import logo from '../src/assets/img/logoSemNome.png'
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 function App() {
   return (
@@ -11,8 +15,40 @@ function App() {
       </div>
 
       <div className='container-lateral'>
+        <div className='container-lateral-titulo'>
+          <h2>Refine sua busca aqui!</h2>
+        </div>
+        <div className='filtros'>
+          <FormControl fullWidth >
+            <InputLabel id="input-municipios">Municípios</InputLabel>
+            <Select
+              labelId="select-municipios"
+              id="municipios"
+              label="Municípios"
+            >
+              <MenuItem >Ten</MenuItem>
+              <MenuItem >Twenty</MenuItem>
+              <MenuItem >Thirty</MenuItem>
+
+            </Select>
+            <InputLabel id="input-municipio">Município</InputLabel>
+            <Select fullWidth
+              labelId="select-municipio"
+              id="municipio"
+              label="Município"
+            >
+              <MenuItem >Ten</MenuItem>
+              <MenuItem >Twenty</MenuItem>
+              <MenuItem >Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </div>
+
+      <div className='container-table'>
 
       </div>
+
     </div>
   );
 }
