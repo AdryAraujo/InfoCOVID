e9eb6849bb7cdf9ba4548a235b735fbfe38cb82e
Adryelle Araujo <adrytvd@gmail.com>
2023-06-24 17:24:32 -0300

    adicionando cabeçalho do site

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import * as React from 'react';
+import logo from '../src/assets/img/logoSemNome.png'
 
 function App() {
   return (
-    <div className="App">
-      <h1>olá mundo</h1>
+    <div className='container'>
+      <div className='cabecalho'>
+        <img id="logo" src={logo} alt='Info Covid'/>
+        <h1>INFO COVID</h1>
       </div>
+
+      <div className='container-lateral'>
+
+      </div>
+    </div>
   );
 }
 
