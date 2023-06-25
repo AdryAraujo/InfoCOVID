3f02e2842405fc82b67f0cd1426f8996853e74b2
LeoDant4s <118023712+LeoDant4s@users.noreply.github.com>
2023-06-25 17:27:46 -0300

    Adição de função na API

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -14,3 +14,7 @@ export async function getCities(state) {
 export async function getStates() {
     return api.get('/state/');
 }
+
+export async function getCases(dataInicio, dataFim, estado, municipio, campo, maiorQue) {
+    return api.get(`/cases?dataInicio=${dataInicio}&dataFim=${dataFim}&estado=${estado}&municipio=${municipio}&campo=${campo}&maiorQue=${maiorQue}`);
+}
