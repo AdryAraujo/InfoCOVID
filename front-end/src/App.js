0236d3a1ad4737902e11cf4d5889f2177ce84313
Adryelle Araujo <adrytvd@gmail.com>
2023-06-24 22:25:41 -0300

    tabela de dados

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,16 +16,49 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(uf,
+  cod_IBGE,
+  municipio,
+  data_de_coleta,
+  tipo_de_local,
+  confirm_acum,
+  confirmados_acum_100k_hab,
+  confirm_dia,
+  semana_epidem,
+  obitos_acum,
+  obitos_dia,
+  obitos_por_conf,
+  pop_esti_2019,
+  ultima_atuali,
+  dado_repetido,
+  popu_estimada,
+  data_ref,
+  num_atuali) {
+  return { uf,
+    cod_IBGE,
+    municipio,
+    data_de_coleta,
+    tipo_de_local,
+    confirm_acum,
+    confirmados_acum_100k_hab,
+    confirm_dia,
+    semana_epidem,
+    obitos_acum,
+    obitos_dia,
+    obitos_por_conf,
+    pop_esti_2019,
+    ultima_atuali,
+    dado_repetido,
+    popu_estimada,
+    data_ref,
+    num_atuali};
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('SE', 2800308, "Aracaju", "2020-04-02T03:00:00.000Z", "city", 20, 3, "20", "202014", 2, 2, "0.1", 657013, 0, 0, 0, 664908, "2020-04-02T03:00:00.000Z", 20,),
+  createData('SE', 2800308, "Aracaju", "2020-04-02T03:00:00.000Z", "city", 20, 3, "20", "202014", 2, 2, "0.1", 657013, 0, 0, 0, 664908, "2020-04-02T03:00:00.000Z", 20,),
+  createData('SE', 2800308, "Aracaju", "2020-04-02T03:00:00.000Z", "city", 20, 3, "20", "202014", 2, 2, "0.1", 657013, 0, 0, 0, 664908, "2020-04-02T03:00:00.000Z", 20,),
+  createData('SE', 2800308, "Aracaju", "2020-04-02T03:00:00.000Z", "city", 20, 3, "20", "202014", 2, 2, "0.1", 657013, 0, 0, 0, 664908, "2020-04-02T03:00:00.000Z", 20,)
 ];
 
 function App() {
@@ -136,15 +169,29 @@ function App() {
           </div>
           <div className='container-table'>
           <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <Table aria-label="simple table">
                 <TableHead>
                   <TableRow>
-                    <TableCell>Dessert (100g serving)</TableCell>
-                    <TableCell align="right">Calories</TableCell>
-                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    <TableCell>UF</TableCell>
+                    <TableCell align="right">Cod. IBGE</TableCell>
+                    <TableCell align="right">Município</TableCell>
+                    <TableCell align="right">Data de coleta</TableCell>
+                    <TableCell align="right">Tipo de local</TableCell>
+                    <TableCell align="right">Confirmados acumulado</TableCell>
+                    <TableCell align="right">Confirmados no dia</TableCell>
+                    <TableCell align="right">Confirmados acumulado/100k de hab.</TableCell>
+                    <TableCell align="right">Semana epidemológica</TableCell>
+                    <TableCell align="right">Óbitos acumulados</TableCell>
+                    <TableCell align="right">Óbitos no dia</TableCell>
+                    <TableCell align="right">Óbitos/Confirmados</TableCell>
+                    <TableCell align="right">População estimada 2019</TableCell>
+                    <TableCell align="right">Última atualização</TableCell>
+                    <TableCell align="right">Dado repetido</TableCell>
+                    <TableCell align="right">População estimada</TableCell>
+                    <TableCell align="right">Data referência</TableCell>
+                    <TableCell align="right">Número da atualização</TableCell>
                   </TableRow>
+
                 </TableHead>
                 <TableBody>
                   {rows.map((row) => (
@@ -153,12 +200,28 @@ function App() {
                       sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                     >
                       <TableCell component="th" scope="row">
-                        {row.name}
+                        {row.uf}
                       </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
+                      <TableCell align="right">{row.cod_IBGE}</TableCell>
+                      <TableCell align="right">{row.municipio}</TableCell>
+                      <TableCell align="right">{row.data_de_coleta}</TableCell>
+                      <TableCell align="right">{row.tipo_de_local}</TableCell>
+                      <TableCell align="right">{row.confirm_acum}</TableCell>
+                      <TableCell align="right">{row.confirm_dia}</TableCell>
+                      <TableCell align="right">{row.confirmados_acum_100k_hab}</TableCell>
+                      <TableCell align="right">{row.semana_epidem}</TableCell>
+                      <TableCell align="right">{row.obitos_acum}</TableCell>
+                      <TableCell align="right">{row.obitos_dia}</TableCell>
+                      <TableCell align="right">{row.obitos_por_conf}</TableCell>
+                      <TableCell align="right">{row.pop_esti_2019}</TableCell>
+                      <TableCell align="right">{row.ultima_atuali}</TableCell>
+                      <TableCell align="right">{row.dado_repetido}</TableCell>
+                      <TableCell align="right">{row.popu_estimada}</TableCell>
+                      <TableCell align="right">{row.data_ref}</TableCell>
+                      <TableCell align="right">{row.num_atuali}</TableCell>
+
+
+
                     </TableRow>
                   ))}
                 </TableBody>
