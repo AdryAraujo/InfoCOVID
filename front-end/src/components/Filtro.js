e5222fe42db6a10b618256ae62f83b85716c16a4
Adryelle Araujo <adrytvd@gmail.com>
2023-06-25 21:31:45 -0300

    adicionando mais filtros

--- front-end/src/components/Filtro.js
+++ front-end/src/components/Filtro.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { FormControl, FormLabel, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { Button, FormControl, FormLabel, InputLabel, MenuItem, OutlinedInput, Select, TextField } from "@mui/material";
 import { getCities, getStates } from '../services/api';
 
 
-function Filtro(){
+function Filtro() {
     const [states, setStates] = useState([]);
     const [cities, setCities] = useState([]);
     const [selectedState, setSelectedState] = useState('');
@@ -12,20 +12,20 @@ function Filtro(){
     async function loadStates() {
         const response = await getStates();
         if (response.data) {
-        setStates(response.data);
+            setStates(response.data);
         }
     }
 
     async function loadCities() {
         if (!selectedState) {
-        return;
+            return;
         }
 
         const response = await getCities(selectedState);
         if (response.data) {
-        const cities = response.data.filter((city) => city.municipio !== null);
-        setCities(cities);
-        setSelectedCity('');
+            const cities = response.data.filter((city) => city.municipio !== null);
+            setCities(cities);
+            setSelectedCity('');
         }
     }
 
@@ -36,15 +36,16 @@ function Filtro(){
     useEffect(() => {
         loadCities();
     }, [selectedState]);
-    return(
+    return (
         <>
             <FormControl fullWidth>
                 <FormLabel id='data-inicio' sx={{ color: 'white' }} >Data Início:</FormLabel>
                 <TextField
                     sx={{
-                    backgroundColor: "white",
-                    color: "#0E4364",
-                    marginBottom: "20px"
+                        backgroundColor: "white",
+                        color: "#0E4364",
+                        marginBottom: "20px",
+                        borderRadius: '4px'
                     }}
                     id="dataInicio"
                     fullWidth
@@ -56,9 +57,10 @@ function Filtro(){
                 <FormLabel id='data-fim' sx={{ color: 'white' }} >Data Fim:</FormLabel>
                 <TextField
                     sx={{
-                    backgroundColor: "white",
-                    color: "#0E4364",
-                    marginBottom: "20px"
+                        backgroundColor: "white",
+                        color: "#0E4364",
+                        marginBottom: "20px",
+                        borderRadius: '4px'
                     }}
                     id="dataFim"
                     fullWidth
@@ -71,9 +73,9 @@ function Filtro(){
                 <Select
                     value={selectedState}
                     sx={{
-                    backgroundColor: "white",
-                    color: "#0E4364",
-                    marginBottom: "20px"
+                        backgroundColor: "white",
+                        color: "#0E4364",
+                        marginBottom: "20px"
                     }}
                     labelId="select-estados"
                     id="estados"
@@ -82,18 +84,17 @@ function Filtro(){
                     onChange={(e) => setSelectedState(e.target.value)}
                 >
                     {states.map(state => (
-                    <MenuItem value={state['UF']}>{state['UF']}</MenuItem>
+                        <MenuItem value={state['UF']}>{state['UF']}</MenuItem>
                     ))}
                 </Select>
             </FormControl>
-                <FormControl fullWidth >
+            <FormControl fullWidth >
                 <InputLabel id="input-municipios">Municípios</InputLabel>
                 <Select
                     value={selectedCity}
                     sx={{
-                    backgroundColor: "white",
-                    color: "#0E4364",
-                    marginBottom: "20px"
+                        backgroundColor: "white",
+                        marginBottom: "20px"
                     }}
                     labelId="select-municipios"
                     id="municipios"
@@ -101,10 +102,28 @@ function Filtro(){
                     onChange={(e) => setSelectedCity(e.target.value)}
                 >
                     {cities.map(city => (
-                    <MenuItem value={city.municipio}>{city.municipio}</MenuItem>
+                        <MenuItem value={city.municipio}>{city.municipio}</MenuItem>
                     ))}
                 </Select>
-                </FormControl>
+            </FormControl>
+            <FormControl fullWidth >
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <InputLabel id="input-campo">Campo</InputLabel>
+                    <Select fullWidth
+                        sx={{
+                            backgroundColor: "white"
+                        }}
+                        labelId="select-campo"
+                        id="Campo"
+                        label="Campo"
+                    >
+                        <MenuItem>teste</MenuItem>
+                    </Select>
+                    <span style={{marginInline: '2px', color: 'white'}}>{">"}</span>
+                    <TextField fullWidth id="outlined-basic" label="Outlined" variant="outlined" sx={{backgroundColor: 'white', borderRadius: '4px'}}/>
+                </div>
+            </FormControl>
+            <Button fullWidth variant='contained' sx={{backgroundColor: '#3D989B', margin: '18px auto'}}>Filtrar</Button>
         </>
     );
 }
