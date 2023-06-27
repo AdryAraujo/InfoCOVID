01b08d41bcf7fe1d5aeb2c29008e3dcf895092ed
Lucas Andrade Cruz <ddlukas0@gmail.com>
2023-06-27 10:45:15 -0300

    Adiciona header de controle no axios

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
     },
     crossdomain: true,
 });
