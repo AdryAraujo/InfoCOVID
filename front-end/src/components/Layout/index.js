784867b48af3c683982d0035013178c062c93b4c
Lucas Andrade Cruz <ddlukas0@gmail.com>
2023-08-21 17:56:50 -0300

    Adiciona navegação para o mapa no header

--- front-end/src/components/Layout/index.js
+++ front-end/src/components/Layout/index.js
@@ -1,17 +1,34 @@
+import HomeIcon from '@mui/icons-material/Home';
+import MapIcon from '@mui/icons-material/Map';
+import { Button } from '@mui/material';
 import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 import logo from '../../assets/img/logoSemNome.png';
-import Filtro from '../Filtro';
-import InfoCards from '../InfoCards';
-import InfoTable from '../InfoTable';
-import { Outlet } from 'react-router-dom';
 import './styles.css';
 
 function Layout() {
     return (
         <div className='container'>
             <div className='cabecalho'>
-                <img id="logo" src={logo} alt='Info Covid' />
-                <h1>INFO COVID</h1>
+                <Link to={'/'}>
+                    <div className="cabecalho-logo">
+                        <img id="logo" src={logo} alt='Info Covid' />
+                        <h1>INFO COVID</h1>
+                    </div>
+                </Link>
+
+                <div className="cabecalho-links">
+                    <Link to={'/'}>
+                        <Button startIcon={<HomeIcon />} variant='contained'>
+                            Home
+                        </Button>
+                    </Link>
+                    <Link to={'/acessos'}>
+                        <Button startIcon={<MapIcon />} variant='contained'>
+                            Mapa de Acessos
+                        </Button>
+                    </Link>
+                </div>
             </div>
             <Outlet />
         </div>
